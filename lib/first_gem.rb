19b3298bbd29ca698d062c9807d8c829d2edbdd2
vishal sharma <vs9390@gmail.com>
2017-02-23 08:43:21 +0000

    coverall added

--- lib/first_gem.rb
+++ lib/first_gem.rb
@@ -1,4 +1,5 @@
 require "first_gem/version"
+require "first_gem/hello"
 
 module FirstGem
   # Your code goes here...
--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -1,7 +1,8 @@
-describe "test one" do
+describe Hello do
     context "first test" do
         it "is a test" do
-            expect(1).to eql(1)
+            @hello = Hello.new
+            @hello.hello
         end
     end
 end
